f4499b121b85d9071ea351e8dca4c1bab08454a7
zolko <zolko1997@hotmail.hu>
2020-11-15 16:18:41 +0100

    loaded index.html as default page

--- src/main/java/hu/unideb/pokemondatabase/PokemonDatabaseApplication.java
+++ src/main/java/hu/unideb/pokemondatabase/PokemonDatabaseApplication.java
@@ -2,8 +2,9 @@ package hu.unideb.pokemondatabase;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class PokemonDatabaseApplication {
 
 	public static void main(String[] args) {
