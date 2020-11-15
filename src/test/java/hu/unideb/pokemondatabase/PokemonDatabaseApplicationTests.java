a30c8ac283e27c3845f5b45facd8eef07bb6ae1b
zolko <zolko1997@hotmail.hu>
2020-11-15 15:51:21 +0100

    initial commit

---
+++ src/test/java/hu/unideb/pokemondatabase/PokemonDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.unideb.pokemondatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
